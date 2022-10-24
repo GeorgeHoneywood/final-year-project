7158c9734984817f30fadfe1dfac5ba456fbef62
Honeywood George (2019) ZHAC152 <george.honeywood.2019@live.rhul.ac.uk>
2022-10-24 17:02:54 +0100

    feat: projecting an array of coordinates to web mercator, and rendering to the canvas

--- proof-of-concepts/2-rendering-a-way/demo.js
+++ proof-of-concepts/2-rendering-a-way/demo.js
@@ -1,20 +1,50 @@
-import { getData } from "./load.js";
+import { getCoordinates } from "./load.js";
 
 const canvas = document.getElementById("map");
-
 const ctx = canvas.getContext("2d");
 
-function drawBasicBox(ctx) {
-    ctx.fillStyle = '#000';
-    ctx.strokeStyle = '#FFF';
+// coordinates is an array of longitude (λ), latitude (𝜙) wgs84 pairs
+const coordinates = await getCoordinates("/baden-baden.geojson")
 
-    ctx.fillRect(0, 0, 600, 10);
-    ctx.strokeRect(0, 0, 600, 10);
+// radius of the earth, in meters
+// const R = 6371204;
+
+const RADIANS_TO_DEGREES = 180 / Math.PI;
+const PI_TIMES_4 = Math.PI * 4;
+const web_mercator_coordinates = [];
+for (const [long, lat] of coordinates) {
+    // equations from: 
+    // calculate x/long/λ
+    // const x = (Math.PI * R * long) / 180;
+    const x = long;
+    // calculate y/lat/𝜙
+    // const y = R * Math.log(Math.tan(45 + (lat / 2)));
+    // function lat2y(lat) { return Math.log(Math.tan((lat / 90 + 1) * PI_4 )) * RAD2DEG; }
+    // const y = Math.log(Math.tan((lat / 90 + 1) * PI_TIMES_4)) * RADIANS_TO_DEGREES;
+    const y = Math.log(Math.tan((lat / (180 / Math.PI)) / 2 + Math.PI / 4)) * RADIANS_TO_DEGREES;
+    // console.log(`long=${long}, lat=${lat}, x=${x}, y=${y}`);
+    web_mercator_coordinates.push([x, y]);
 }
 
-drawBasicBox(ctx);
+const x_coordinates = web_mercator_coordinates.map(pair => pair[0]);
+const y_coordinates = web_mercator_coordinates.map(pair => pair[1]);
 
-const data = await getData("/egham.geojson")
-const coordinates = data.features[0].geometry.coordinates[0];
+const minimum_x = Math.min(...x_coordinates);
+const mininum_y = Math.min(...y_coordinates);
 
-console.log("coordinates is an array of lat/long wgs84 pairs", coordinates)
+const scaled_coordinates = [];
+
+for (const [x, y] of web_mercator_coordinates) {
+    scaled_coordinates.push([x - minimum_x, y - mininum_y]);
+}
+
+const height = canvas.height;
+
+ctx.beginPath();
+// ctx.moveTo(...scaled_coordinates[0].map(v => (v * 800)))
+for (const [x, y] of scaled_coordinates) {
+    ctx.lineTo(x * 2000, height - (y * 2000));
+}
+ctx.stroke();
+
+// console.log(scaled_coordinates);
--- proof-of-concepts/2-rendering-a-way/load.js
+++ proof-of-concepts/2-rendering-a-way/load.js
@@ -1,11 +1,14 @@
-const getData = async (url) => {
+const getCoordinates = async (url) => {
     const resp = await fetch(url);
     if (!resp.ok) {
         throw new Error(
             `request failed; error code: ${resp.statusText || resp.status}`
         );
     }
-    return resp.json();
+    const data = await resp.json()
+    // get the coordinates for the first feature in a geojson file
+    // only works for Polygon type geometries
+    return data.features[0].geometry.coordinates[0];
 };
 
-export { getData }
+export { getCoordinates }
