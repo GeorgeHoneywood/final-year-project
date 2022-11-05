a2f7f08c7fd63f779f897a184034e2a3a867512b
GeorgeHoneywood <george.honeywood.2019@live.rhul.ac.uk>
2022-11-05 23:45:23 +0000

    fix: don't upload node_modules to web server

--- proof-of-concepts/upload.sh
+++ proof-of-concepts/upload.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-rsync -zavh "$1" "git.honeyfox.uk:/var/www/html/files.george.honeywood.org.uk/$1"
+rsync -zavh --exclude "node_modules" "$1" "git.honeyfox.uk:/var/www/html/files.george.honeywood.org.uk/$1"
