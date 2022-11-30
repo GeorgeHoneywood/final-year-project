cc8fb045525d4bf8d631b24f8299448c1c8ebe30
GeorgeHoneywood <george.honeywood.2019@live.rhul.ac.uk>
2022-11-30 17:15:07 +0000

    report: software engineering & literature review

--- proof-of-concepts/upload.sh
+++ proof-of-concepts/upload.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-rsync -zavh --exclude "node_modules" "$1" "git.honeyfox.uk:/var/www/html/files.george.honeywood.org.uk/$1"
+rsync -zavh --exclude "node_modules" --exclude "*.map" "$1" "git.honeyfox.uk:/var/www/html/files.george.honeywood.org.uk/$1"
