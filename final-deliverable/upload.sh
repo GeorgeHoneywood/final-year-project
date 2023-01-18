ac8edf100ea319909dc346fb01f6300f0ead5b1a
Honeywood George (2019) ZHAC152 <george.honeywood.2019@live.rhul.ac.uk>
2023-01-18 22:55:16 +0000

    fix: remove map file from upload script

--- final-deliverable/upload.sh
+++ final-deliverable/upload.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-rsync -zavh --relative "index.html" "dist/" "sw.js" "data/england.map" "git.honeyfox.uk:/var/www/html/files.george.honeywood.org.uk/final-deliverable/"
+rsync -zavh --relative "index.html" "dist/" "sw.js" "git.honeyfox.uk:/var/www/html/files.george.honeywood.org.uk/final-deliverable/"
