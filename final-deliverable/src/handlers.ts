39522398d21afd5d77ae902f9f1719a35377be14
Honeywood George (2019) ZHAC152 <george.honeywood.2019@live.rhul.ac.uk>
2023-01-28 10:19:32 +0000

    refactor: move event handler code out of `main.ts`

---
+++ final-deliverable/src/handlers.ts
@@ -0,0 +1,266 @@
+import type { CanvasMap } from "./map.js";
+import type { Coord } from "./types.js";
+
+// window for second tap to occur to be considered double tap 
+const DOUBLE_TAP_TIMEOUT = 300;
+
+/**
+ * registers event handlers, that allow the user to control the CanasMap
+ * @param map 
+ * @param canvas 
+ * @param geolocate_button 
+ */
+function registerEventHandlers(
+    map: CanvasMap,
+    canvas: HTMLCanvasElement,
+    geolocate_button: HTMLButtonElement,
+) {
+    // register mouse event handlers 
+    handleMouse(map, canvas);
+    // register touch event handlers
+    handleTouch(map, canvas);
+    // register keyboard event handlers for WASD+- controls
+    handleKeyboard(map);
+
+    // register geolocate button
+    handleGPS(map, geolocate_button);
+
+    // handle window resizes
+    window.addEventListener("resize", (e) => {
+        // TODO: this should adjust offsets so that the centre of the map
+        // stays in the centre when the window resizes
+        e.preventDefault();
+
+        map.setDirty();
+    });
+}
+
+function handleTouch(map: CanvasMap, canvas: HTMLCanvasElement) {
+    // touch handling code: https://developer.mozilla.org/en-US/docs/Web/API/Touch_events
+    // TODO:
+    // * Double tap zoom
+
+    const currentTouches: any[] = [];
+    let previousPinchDistance: number | null = null;
+    let pinchCenter: Coord | null = null;
+    let firstTap = false;
+    let wasDoubleTapped = false;
+    let doubleTapPosition: Coord | null = null;
+    let previousDoubleTapDistance: number | null = null;
+
+    function copyTouch({ identifier, pageX, pageY }: any) {
+        return { identifier, pageX, pageY };
+    }
+
+    function getCurrentTouchIndex(idToFind: any) {
+        for (let i = 0; i < currentTouches.length; i++) {
+            const id = currentTouches[i].identifier;
+
+            if (id === idToFind) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    canvas.addEventListener("touchstart", (e) => {
+        e.preventDefault();
+
+        const touches = e.changedTouches;
+
+        for (let i = 0; i < touches.length; i++) {
+            currentTouches.push(copyTouch(touches[i]));
+        }
+
+        // figure out if a double tap has occurred
+        if (!firstTap) {
+            firstTap = true;
+            setTimeout(() => { firstTap = false; }, DOUBLE_TAP_TIMEOUT);
+            return;
+        }
+
+        if (firstTap) {
+            wasDoubleTapped = true;
+        }
+    });
+
+    canvas.addEventListener("touchmove", (e) => {
+        e.preventDefault();
+
+        const touches = e.changedTouches;
+
+        if (wasDoubleTapped && e.touches.length === 1) {
+            // handle double tap zoom first
+            const rect = canvas.getBoundingClientRect();
+            doubleTapPosition ??= {
+                x: touches[0].pageX - rect.left,
+                y: rect.bottom - touches[0].pageY,
+            };
+
+            // we only care about the distance in the y-axis for double taps
+            const doubleTapDistance = doubleTapPosition.y - rect.bottom - touches[0].pageY;
+            previousDoubleTapDistance ??= doubleTapDistance;
+
+            // zoom about the position of the double tap
+            map.zoom((previousDoubleTapDistance - doubleTapDistance) / 100, doubleTapPosition);
+            previousDoubleTapDistance = doubleTapDistance;
+        } else if (touches.length === 1) {
+            // handle single-finger scrolling
+            // this handles the random single touches that occur during a pinch zoom
+            if (previousPinchDistance !== null)
+                return;
+
+            const idx = getCurrentTouchIndex(touches[0].identifier);
+
+            if (idx >= 0) {
+                map.translate({
+                    x: -(currentTouches[idx].pageX - touches[0].pageX),
+                    y: currentTouches[idx].pageY - touches[0].pageY,
+                });
+                currentTouches.splice(idx, 1, copyTouch(touches[0]));
+            }
+
+
+        } else if (touches.length === 2) {
+            // handle pinch zoom
+            const pinchDistance = Math.hypot(
+                (touches[0].pageX - touches[1].pageX),
+                (touches[0].pageY - touches[1].pageY)
+            );
+
+            previousPinchDistance ??= pinchDistance;
+
+            const rect = canvas.getBoundingClientRect();
+            pinchCenter ??= {
+                x: ((touches[0].pageX - rect.left)
+                    + (touches[1].pageX - rect.left)) / 2,
+                y: ((rect.bottom - touches[0].pageY)
+                    + (rect.bottom - touches[1].pageY)) / 2,
+            };
+
+            // zoom about the pinch center
+            map.zoom(-(previousPinchDistance - pinchDistance) / 100, pinchCenter);
+
+            previousPinchDistance = pinchDistance;
+        }
+    });
+    canvas.addEventListener("touchend", (e) => {
+        e.preventDefault();
+
+        const touches = e.changedTouches;
+
+        for (let i = 0; i < touches.length; i++) {
+
+            const idx = getCurrentTouchIndex(touches[i].identifier);
+
+            if (idx >= 0) {
+                currentTouches.splice(idx, 1);
+            }
+        }
+
+        // if we are pinch zooming, only stop once the last touch event has ended.
+        // otherwise we get a a map jump when you remove one finger
+        if (currentTouches.length === 0) {
+            previousPinchDistance = null;
+            pinchCenter = null;
+        }
+
+        // clear out double tap stuff
+        wasDoubleTapped = false;
+        previousDoubleTapDistance = null;
+        doubleTapPosition = null;
+    });
+
+    canvas.addEventListener("touchcancel", (e) => {
+        console.log("noop");
+    });
+}
+
+function handleKeyboard(map: CanvasMap) {
+    document.addEventListener("keydown", (e) => {
+        switch (e.key) {
+            case "w":
+                e.preventDefault();
+                map.translate({ x: 0, y: -20 });
+                break;
+            case "s":
+                e.preventDefault();
+                map.translate({ x: 0, y: 20 });
+                break;
+            case "a":
+                e.preventDefault();
+                map.translate({ x: 20, y: 0 });
+                break;
+            case "d":
+                e.preventDefault();
+                map.translate({ x: -20, y: 0 });
+                break;
+            case "+":
+                e.preventDefault();
+                map.zoom(1);
+                break;
+            case "-":
+                e.preventDefault();
+                map.zoom(-1);
+                break;
+        }
+    });
+}
+
+function handleMouse(map: CanvasMap, canvas: HTMLCanvasElement) {
+    let mousePosition: Coord | null = null;
+
+    canvas.addEventListener("wheel", (e) => {
+        e.preventDefault();
+
+        e.deltaY < 0 ? map.zoom(0.2, mousePosition!) : map.zoom(-0.2, mousePosition!);
+    });
+
+    let mouseDown = false;
+
+    canvas.addEventListener("mousedown", (e) => {
+        e.preventDefault();
+        mouseDown = true;
+    });
+
+    canvas.addEventListener("mouseup", (e) => {
+        e.preventDefault();
+        mouseDown = false;
+    });
+
+    canvas.addEventListener("mouseout", (e) => {
+        e.preventDefault();
+        mouseDown = false;
+    });
+
+    canvas.addEventListener("mousemove", (e) => {
+        e.preventDefault();
+
+        const rect = canvas.getBoundingClientRect();
+        mousePosition = {
+            x: e.clientX - rect.left,
+            y: rect.bottom - e.clientY,
+        };
+
+        if (mouseDown) {
+            map.translate({ x: e.movementX, y: -e.movementY });
+        }
+    });
+}
+
+function handleGPS(map: CanvasMap, geolocate_button: HTMLButtonElement) {
+    geolocate_button.addEventListener("click", () => {
+        console.log("geolocating!");
+        if (navigator.geolocation) {
+            // TODO: handle GeolocationPositionError 
+            // ref: https://developer.mozilla.org/en-US/docs/Web/API/GeolocationPositionError
+            navigator.geolocation.watchPosition((pos: GeolocationPosition) => {
+                map.setUserPosition(pos.coords);
+            });
+        } else {
+            console.log("geolocation is unsupported!");
+        }
+    });
+}
+
+export { registerEventHandlers }
