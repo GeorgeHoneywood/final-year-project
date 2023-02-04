30158319dfa2b0d9c9f5eabef9d6cc6de1780884
Honeywood George (2019) ZHAC152 <george.honeywood.2019@live.rhul.ac.uk>
2023-02-04 10:28:45 +0000

    feat: deploy assets folder

--- final-deliverable/upload.sh
+++ final-deliverable/upload.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-rsync -zavh --relative "index.html" "dist/" "sw.js" "git.honeyfox.uk:/var/www/html/files.george.honeywood.org.uk/final-deliverable/"
+rsync -zavh --relative "index.html" "dist/" "assets/" "sw.js" "git.honeyfox.uk:/var/www/html/files.george.honeywood.org.uk/final-deliverable/"
