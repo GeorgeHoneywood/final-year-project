4b55d0f3ac529243e346b41fa4e2aae46c372b7b
GeorgeHoneywood <george.honeywood.2019@live.rhul.ac.uk>
2023-03-07 08:41:15 +0000

    feat: make website installable, via webmanifest

--- final-deliverable/upload.sh
+++ final-deliverable/upload.sh
@@ -3,4 +3,6 @@
 cd dist || exit
 
 # /data folder contains large map files we don't want to upload
-rsync -zavh --relative "index.html" "assets/" "icons/" "sw.js" "git.honeyfox.uk:/var/www/html/files.george.honeywood.org.uk/final-deliverable/"
+# --delete is okay because it doesn't touch the excludes folder
+# put --include="data/<name>.map" first to upload new map file
+rsync -zavh --relative --delete --exclude "data/" "." "git.honeyfox.uk:/var/www/html/files.george.honeywood.org.uk/final-deliverable/"
